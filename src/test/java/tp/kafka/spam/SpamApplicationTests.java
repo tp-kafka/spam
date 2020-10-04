07d8a0afe4626badbda03a1651359c057f5d56e6
tobias.waller@googlemail.com <tobias.waller@googlemail.com>
2020-10-04 22:36:38 +0000

    wip: import

---
+++ src/test/java/tp/kafka/spam/SpamApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.kafka.spam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
