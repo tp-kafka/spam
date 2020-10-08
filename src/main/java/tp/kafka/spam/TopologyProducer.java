8694bc60bcc8276ac3730f986245ed9d449281bd
tobias.waller@googlemail.com <tobias.waller@googlemail.com>
2020-10-08 09:38:55 +0000

    deserialization

--- src/main/java/tp/kafka/spam/TopologyProducer.java
+++ src/main/java/tp/kafka/spam/TopologyProducer.java
@@ -21,6 +21,7 @@ import org.apache.kafka.streams.state.Stores;
 
 import io.quarkus.kafka.client.serialization.JsonbSerde;
 import lombok.extern.java.Log;
+import org.apache.kafka.common.serialization.Serdes.VoidSerde;
 
 @ApplicationScoped
 @Log
@@ -29,18 +30,22 @@ public class TopologyProducer {
     @Inject
     Configuration conf;
 
+    private final JsonbSerde<ChatMessage> msgSerde = new JsonbSerde<>(ChatMessage.class);
+    private final VoidSerde voidSerde = new VoidSerde();
 
     @Produces
     public Topology filteredInputTopology() {
         StreamsBuilder builder = new StreamsBuilder();
-        builder.<Void, ChatMessage>stream(conf.inputTopic())
+        builder.<Void, ChatMessage>stream(conf.inputTopic(), Consumed.with(voidSerde, msgSerde))
             .filterNot(this::containsBadWords)
             .to(conf.outputTopic());
         return builder.build();
     }
 
     Boolean containsBadWords(Void key, ChatMessage msg){
-        return msg.getMessage().toLowerCase().contains("Fight Club");
+        boolean result = msg.getMessage().toLowerCase().contains("fight club");
+        TopologyProducer.log.info(msg + " does " + (result?"":"not ") +  "contain bad words.");
+        return result;
     }
 
 }
